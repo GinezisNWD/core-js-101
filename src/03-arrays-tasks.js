7059748a432242e606dae708b74e517b7987dd7d
Ginezis <Ginezis25@gmail.com>
2023-06-03 23:10:10 +0300

    add solutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -477,8 +477,8 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return new Array(end - start + 1).fill(start).map((elem, index) => elem + index);
 }
 
 /**
@@ -492,8 +492,8 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return [...new Set(arr)];
 }
 
 /**
@@ -544,8 +544,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map(childrenSelector).flat();
 }
 
 
@@ -584,8 +584,12 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const center = arr.length / 2;
+  if (arr.length % 2 === 0) {
+    return [arr.slice(center), arr.slice(0, center)].flat();
+  }
+  return [arr.slice(center + 1), Math.ceil(center), arr.slice(0, center)].flat();
 }
 
 
--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,14 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+	return new Promise((resolve, reject) => {
+		if (typeof isPositiveAnswer !== 'boolean') {
+			reject(new Error('Wrong parameter is passed! Ask her again.'));
+		}
+		if (isPositiveAnswer === true) { resolve('Hooray!!! She said "Yes"!'); }
+		if (isPositiveAnswer === false) (resolve('Oh no, she said "No".'));
+	});
 }
 
 
@@ -48,8 +54,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+	return Promise.all(array);
 }
 
 /**
@@ -71,8 +77,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+	return Promise.race(array);
 }
 
 /**
@@ -93,12 +99,12 @@ function getFastestPromise(/* array */) {
  *
  */
 function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+	throw new Error('Not implemented');
 }
 
 module.exports = {
-  willYouMarryMe,
-  processAllPromises,
-  getFastestPromise,
-  chainPromises,
+	willYouMarryMe,
+	processAllPromises,
+	getFastestPromise,
+	chainPromises,
 };
